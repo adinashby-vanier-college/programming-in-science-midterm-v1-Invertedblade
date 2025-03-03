a08ef84864d2ebff910b675c3d95e5a1a3a60f6f
Abdullah <rahmana0807@gmail.com>
2025-03-03 09:46:04 -0500

    not completed but I sure do hope i get partial points and pass this midterm

--- Midterm.py
+++ Midterm.py
@@ -2,15 +2,33 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
+    result = round((math.pi) * (radius ** 2), 2)
+    return result
 
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
-    return ""
+    result = ""
+    if n > 4:
+        for i in range(1, n)
+            for j in range(1, i + 1)
+                result += "*"
+    else:
+        return "The pyramid height should be at least 4."
+
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
-    return ""
+    result = ""
+    if n > 3:
+    for i in range(1, n):
+        for j in range(1, i + 1):
+            result += " " + ("*" * (n + 2))
+        result += "\n"
+        n -= 2
+    return result
+else:
+        return "The pyramid height should be at least 3."
+
 
 # ----------------------------------------------------------------
 print(area_of_circle(5))
